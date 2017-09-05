8d5219d328347de7218d882170f643d8279934b3
ukgroupe <ukgroupe@ukgroupes-MacBook.local>
2017-09-05 12:28:18 +0100

    code is working. Tested the button and textfield

--- JsonProject/ViewController.swift
+++ JsonProject/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     @IBOutlet var textfield: UITextField!
     @IBOutlet var label: UILabel!
     @IBAction func button(_ sender: Any) {
-        let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=" + textfield.text!.replacingOccurrences(of: " ", with: "%20") + ",uk&appid=08e64df2d3f3bc0822de1f0fc22fcb2d")!
+        if let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=" + textfield.text!.replacingOccurrences(of: " ", with: "%20") + ",uk&appid=08e64df2d3f3bc0822de1f0fc22fcb2d"){
         
         let request = URLRequest(url: url)
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
@@ -25,8 +25,14 @@ class ViewController: UIViewController {
                     do{
                         
                         let jsondata =  try JSONSerialization.jsonObject(with: jdata, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
-                        print(jsondata)
-                        print(((jsondata["weather"] as? NSArray)?[0] as? NSDictionary)?["description"] as? String)
+                      //  print(jsondata)
+                      //  print(((jsondata["weather"] as? NSArray)?[0] as? NSDictionary)?["description"] as? String)
+                        if let result = (((jsondata["weather"] as? NSArray)?[0] as? NSDictionary)?["description"] as? String){
+                            DispatchQueue.main.async(execute: {
+                                self.label.text = result
+                            })
+                            
+                        }
                     }catch{
                         
                         
@@ -40,8 +46,12 @@ class ViewController: UIViewController {
         
 
     }
+    else{
     
-
+    
+            self.label.text = "sorry... enter another city"
+    }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
